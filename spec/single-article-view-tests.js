dbe6f059b4a6e716669baf30716cc4fcf0975ff9
pflarcology <rhrg500@gmail.com>
2017-01-08 22:39:11 +0000

    single article view instantiates with article

--- spec/single-article-view-tests.js
+++ spec/single-article-view-tests.js
@@ -3,4 +3,18 @@ function testSingleArticleView() {
   assert.isTrue(singleArticleView, "testSingleNoteView")
 }
 
+function testSingleArticleViewHasArticle() {
+  function ArticleDouble() {};
+    ArticleDouble.prototype = {
+      revealHeadline: function() {return "Breaking new stuff"},
+      revealStory: function() {return "Groundbreaking story"}
+  };
+  var articleDouble = new ArticleDouble();
+  var singleArticleView = new SingleArticleView(articleDouble);
+  assert.isTrue(singleArticleView.article === articleDouble, "testSingleArticleViewHasArticle")
+
+
+}
+
 testSingleArticleView();
+testSingleArticleViewHasArticle();
