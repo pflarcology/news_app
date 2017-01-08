653bb6dde1eefbb03c61abb63e8298d957eeb4da
pflarcology <rhrg500@gmail.com>
2017-01-08 22:01:46 +0000

    returns list of articles

--- spec/article-list-view-tests.js
+++ spec/article-list-view-tests.js
@@ -9,5 +9,19 @@ function testArticleListHasModel(){
   assert.isTrue(articleListView.articleListModel == articleListModel, "testArticleListHasModel")
 }
 
+function testArticleListReturnsHtmlList(){
+  function ArticleDouble() {};
+    ArticleDouble.prototype = {
+      revealHeadline: function() {return "Breaking new stuff"},
+      revealStory: function() {return "Groundbreaking story"},
+      _id: 0
+  };
+  var articleDouble = new ArticleDouble();
+  var articleListDouble = {returnList: function(){return [articleDouble]}};
+  var articleListView = new ArticleListView(articleListDouble);
+  assert.isTrue(articleListView.returnsHtmlList() == "<ul><li><div>Breaking new stuff</div><div>Groundbreaking story</div></li></ul>", "testArticleListReturnsHtmlList")
+}
+
 testArticleListStart();
 testArticleListHasModel();
+testArticleListReturnsHtmlList();
--- src/article-list-view.js
+++ src/article-list-view.js
@@ -3,5 +3,15 @@
     this.articleListModel = articleListModel
   }
 
+  ArticleListView.prototype.returnsHtmlList = function(){
+    var output = "<ul>"
+    this.articleListModel.returnList().forEach(function(article){
+      var headline = article.revealHeadline();
+      var story = article.revealStory();
+      output += "<li><div>" + headline + "</div><div>" + story + "</div></li>"
+    })
+    return output + "</ul>"
+  }
+
   exports.ArticleListView = ArticleListView
 })(this);
