ae1ab3cab297224bf2586c2e83d9edf4b837166a
pflarcology <rhrg500@gmail.com>
2017-01-08 22:07:57 +0000

    returns list of multiple articles

--- spec/article-list-view-tests.js
+++ spec/article-list-view-tests.js
@@ -13,8 +13,7 @@ function testArticleListReturnsHtmlList(){
   function ArticleDouble() {};
     ArticleDouble.prototype = {
       revealHeadline: function() {return "Breaking new stuff"},
-      revealStory: function() {return "Groundbreaking story"},
-      _id: 0
+      revealStory: function() {return "Groundbreaking story"}
   };
   var articleDouble = new ArticleDouble();
   var articleListDouble = {returnList: function(){return [articleDouble]}};
@@ -22,6 +21,23 @@ function testArticleListReturnsHtmlList(){
   assert.isTrue(articleListView.returnsHtmlList() == "<ul><li><div>Breaking new stuff</div><div>Groundbreaking story</div></li></ul>", "testArticleListReturnsHtmlList")
 }
 
+function testArticleListReturnsHtmlListWithMultipleArticles(){
+  function ArticleDouble() {};
+    ArticleDouble.prototype = {
+      revealHeadline: function() {return "Breaking new stuff"},
+      revealStory: function() {return "Groundbreaking story"}
+  };
+  var articleDouble = new ArticleDouble();
+  var articleDouble2 = new ArticleDouble();
+  var articleListDouble = {returnList: function(){return [articleDouble, articleDouble2]}};
+  var articleListView = new ArticleListView(articleListDouble);
+  assert.isTrue(articleListView.returnsHtmlList() == "<ul><li><div>Breaking new stuff</div><div>Groundbreaking story</div></li><li><div>Breaking new stuff</div><div>Groundbreaking story</div></li></ul>", "testArticleListReturnsHtmlListWithMultipleArticles")
+}
+
+
+
+
 testArticleListStart();
 testArticleListHasModel();
 testArticleListReturnsHtmlList();
+testArticleListReturnsHtmlListWithMultipleArticles();
